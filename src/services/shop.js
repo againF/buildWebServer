1cb6d6c2255b04b959427b99b5e57bece55567c9
Wu YeFei <223056884@gehealthcare.com>
2025-07-08 14:10:32 +0800

    feat: 服务层提供了店铺管理所需的基础业务逻辑，存储暂时以内存和延时模拟

---
+++ src/services/shop.js
@@ -0,0 +1,72 @@
+// 商店数据
+const memoryStorage = {
+    '1001': {
+        name: '苹果'
+    },
+    '1002': {
+        name:'特斯拉'
+    },
+    '1003': {
+        name: '小米'
+    },
+    '1004': {
+        name: '华为'
+    }
+}
+
+// 模拟延时
+async function delay(ms = 300) {
+    await new Promise((r) => setTimeout(r, ms));
+}
+
+class ShopService {
+    async init() {
+        await delay();
+    }
+
+    async find({id,pageIndex = 0, pageSize = 10}) {
+        await delay();
+        if(id){
+            return [memoryStorage[id]].filter(Boolean);
+        }
+
+        return Object.keys(memoryStorage)
+        .slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
+        .map(id => ({
+            id,
+            ...memoryStorage[id]
+        }))
+    }
+
+    async modify({id, values}){
+        await delay();
+
+        const target = memoryStorage[id];
+        if(!target){
+            return null;
+        }
+
+        return Object.assign(target, values);
+    }
+
+    async remove({id}){
+        await delay();
+
+        const target = memoryStorage[id];
+        if(!target){
+            return false;
+        }
+
+        return delete memoryStorage[id];
+    }
+}
+
+// 单例
+let service;
+module.exports = async function (){
+    if(!service){
+        service = new ShopService();
+        await service.init();
+    }
+    return service;
+}
