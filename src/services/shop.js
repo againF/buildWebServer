409bc3b31005129c6ea6427efbb655f68c539b3a
Wu YeFei <223056884@gehealthcare.com>
2025-07-09 14:46:13 +0800

    feat: 数据库访问逻辑

--- src/services/shop.js
+++ src/services/shop.js
@@ -1,70 +1,81 @@
 // 商店数据
-const memoryStorage = {
-    '1001': {
-        name: '苹果'
-    },
-    '1002': {
-        name:'特斯拉'
-    },
-    '1003': {
-        name: '小米'
-    },
-    '1004': {
-        name: '华为'
-    }
-}
+// const memoryStorage = {
+//     '1001': {
+//         name: '苹果'
+//     },
+//     '1002': {
+//         name:'特斯拉'
+//     },
+//     '1003': {
+//         name: '小米'
+//     },
+//     '1004': {
+//         name: '华为'
+//     }
+// }
 
 // 模拟延时
-async function delay(ms = 300) {
-    await new Promise((r) => setTimeout(r, ms));
-}
-
+// async function delay(ms = 300) {
+//     await new Promise((r) => setTimeout(r, ms));
+// }
+const {Shop} = require('../models');
 class ShopService {
     async init() {
-        await delay();
+        // await delay();
     }
     async create({values}){
-        await delay();
-        const id = String(1 + Object.keys(memoryStorage).reduce((m,id)=> Math.max(m,id), -Infinity));
-        return {
-            id,
-            ...(memoryStorage[id] = values)
-        }
+        // await delay();
+        // const id = String(1 + Object.keys(memoryStorage).reduce((m,id)=> Math.max(m,id), -Infinity));
+        // return {
+        //     id,
+        //     ...(memoryStorage[id] = values)
+        // }
+        return await Shop.create(values);
     }
     async find({id,pageIndex = 0, pageSize = 10}) {
-        await delay();
+        // await delay();
         if(id){
-            return [memoryStorage[id]].filter(Boolean);
+            // return [memoryStorage[id]].filter(Boolean);
+            return [await Shop.findByPk(id)]
         }
 
-        return Object.keys(memoryStorage)
-        .slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
-        .map(id => ({
-            id,
-            ...memoryStorage[id]
-        }))
+        // return Object.keys(memoryStorage)
+        // .slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
+        // .map(id => ({
+        //     id,
+        //     ...memoryStorage[id]
+        // }))
+        return await Shop.findAll({
+            offset: pageIndex * pageSize,
+            limit: pageSize
+        })
     }
 
     async modify({id, values}){
-        await delay();
+        // await delay();
 
-        const target = memoryStorage[id];
+        // const target = memoryStorage[id];
+        const target = await Shop.findByPk(id);
         if(!target){
             return null;
         }
 
-        return Object.assign(target, values);
+        // return Object.assign(target, values);
+        Object.assign(target, values);
+        return await target.save();
     }
 
     async remove({id}){
-        await delay();
+        // await delay();
 
-        const target = memoryStorage[id];
+        // const target = memoryStorage[id];
+        const target = await Shop.findByPk(id);
         if(!target){
             return false;
         }
 
-        return delete memoryStorage[id];
+        // return delete memoryStorage[id];
+        return target.destroy();
     }
 }
 
